e551e532cea53ffb1bfd1490b7645051f11fcbab
Oscar Olazabal <oscarolazabal.dev@gmail.com>
2019-12-14 14:22:32 -0800

    init

---
+++ build/precache-manifest.a8888cc08b34fe47204d1b8d828810a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4cc3628c8eea7797ac6992073bdde2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c628f47736001633207",
+    "url": "/static/css/3.5d6599f3.chunk.css"
+  },
+  {
+    "revision": "118b3a13c9e7c2fcf147",
+    "url": "/static/css/main.94ca3ac4.chunk.css"
+  },
+  {
+    "revision": "20c55cb8db5a1457bf83",
+    "url": "/static/js/2.269f1de2.chunk.js"
+  },
+  {
+    "revision": "5c628f47736001633207",
+    "url": "/static/js/3.9e9fccb0.chunk.js"
+  },
+  {
+    "revision": "0f8edd880d4dcf495275",
+    "url": "/static/js/4.df5209b5.chunk.js"
+  },
+  {
+    "revision": "118b3a13c9e7c2fcf147",
+    "url": "/static/js/main.40e2b956.chunk.js"
+  },
+  {
+    "revision": "03396d0ca3aafcb3f45e",
+    "url": "/static/js/runtime~main.315dc23e.js"
+  },
+  {
+    "revision": "f066ce84ef91d85deda32f51f1ebd1ec",
+    "url": "/static/media/flow_plain.f066ce84.png"
+  }
+]);
---
+++ build/static/js/4.df5209b5.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{60:function(n,e,t){"use strict";t.r(e);var o=t(0),u=t.n(o);e.default=function(){return u.a.createElement("p",null,"Not found")}}}]);
+//# sourceMappingURL=4.df5209b5.chunk.js.map
